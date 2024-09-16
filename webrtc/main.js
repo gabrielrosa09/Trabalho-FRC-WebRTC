0f11b82ae19a8f5346ccef4d4dba210ab83ab1e9
Ana Caroline Rocha <sqscamposuni@gmail.com>
2024-09-15 21:20:34 -0300

    add answerButton

--- webrtc/main.js
+++ webrtc/main.js
@@ -105,7 +105,41 @@ callButton.onclick = async () => {
   hangupButton.disabled = false;
 };
 
-/**************** parte da ana vai aqui ******************/
+answerButton.onclick = async () => {
+  const callId = callInput.value;
+  const callDoc = firestore.collection('calls').doc(callId);
+  const answerCandidates = callDoc.collection('answerCandidates');
+  const offerCandidates = callDoc.collection('offerCandidates');
+
+  pc.onicecandidate = (event) => {
+    event.candidate && answerCandidates.add(event.candidate.toJSON());
+  };
+
+  const callData = (await callDoc.get()).data();
+
+  const offerDescription = callData.offer;
+  await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+  const answerDescription = await pc.createAnswer();
+  await pc.setLocalDescription(answerDescription);
+
+  const answer = {
+    type: answerDescription.type,
+    sdp: answerDescription.sdp,
+  };
+
+  await callDoc.update({ answer });
+
+  offerCandidates.onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      console.log(change);
+      if (change.type === 'added') {
+        let data = change.doc.data();
+        pc.addIceCandidate(new RTCIceCandidate(data));
+      }
+    });
+  });
+};
 
 /**************** Aqui é a parte do chat ******************/
 
